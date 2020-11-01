a6a51e03ca4dbc382aa271e79925871d96960496
f94baa176 <f94baasd176ing@gmail.com>
2020-11-01 15:58:01 +0800

    memeber靜態樣式完成

--- src/member.js
+++ src/member.js
@@ -22,7 +22,9 @@ $(function () {
             $('.nav').addClass('hide')
         }
     });
+});
 
+$(function () {
     //大頁簽
     $("a.memberTab").on("click", function(e){
         e.preventDefault();
@@ -93,7 +95,8 @@ $(function () {
             $("div.memberTab").removeClass("-on");
         }
     })
-
+});
+$(function () {
     //燈箱
     // 開啟 Modal 彈跳視窗
     $(".mainBtn_3").on("click", function(){
@@ -114,135 +117,15 @@ $(function () {
             $(".lightbox-block3").removeClass("-openbox -opacity-zero");
         }, 1000);
     });
+});
 
-    //bubble按鈕
-    // $('.button--bubble').each(function () {
-    //     var $circlesTopLeft = $(this).parent().find('.circle.top-left');
-    //     var $circlesBottomRight = $(this).parent().find('.circle.bottom-right');
-    
-    //     var tl = new TimelineMax();
-    //     var tl2 = new TimelineMax();
-    
-    //     var btTl = new TimelineMax({
-    //         paused: true
-    //     });
-    
-    //     tl.to($circlesTopLeft, 1.2, {
-    //         x: -25,
-    //         y: -25,
-    //         scaleY: 2,
-    //         ease: SlowMo.ease.config(0.1, 0.7, false)
-    //     });
-    //     tl.to($circlesTopLeft.eq(0), 0.1, {
-    //         scale: 0.2,
-    //         x: '+=6',
-    //         y: '-=2'
-    //     });
-    //     tl.to($circlesTopLeft.eq(1), 0.1, {
-    //         scaleX: 1,
-    //         scaleY: 0.8,
-    //         x: '-=10',
-    //         y: '-=7'
-    //     }, '-=0.1');
-    //     tl.to($circlesTopLeft.eq(2), 0.1, {
-    //         scale: 0.2,
-    //         x: '-=15',
-    //         y: '+=6'
-    //     }, '-=0.1');
-    //     tl.to($circlesTopLeft.eq(0), 1, {
-    //         scale: 0,
-    //         x: '-=5',
-    //         y: '-=15',
-    //         opacity: 0
-    //     });
-    //     tl.to($circlesTopLeft.eq(1), 1, {
-    //         scaleX: 0.4,
-    //         scaleY: 0.4,
-    //         x: '-=10',
-    //         y: '-=10',
-    //         opacity: 0
-    //     }, '-=1');
-    //     tl.to($circlesTopLeft.eq(2), 1, {
-    //         scale: 0,
-    //         x: '-=15',
-    //         y: '+=5',
-    //         opacity: 0
-    //     }, '-=1');
-    
-    //     var tlBt1 = new TimelineMax();
-    //     var tlBt2 = new TimelineMax();
-    
-    //     tlBt1.set($circlesTopLeft, {
-    //         x: 0,
-    //         y: 0,
-    //         rotation: -45
-    //     });
-    //     tlBt1.add(tl);
-    
-    //     tl2.set($circlesBottomRight, {
-    //         x: 0,
-    //         y: 0
-    //     });
-    //     tl2.to($circlesBottomRight, 1.1, {
-    //         x: 30,
-    //         y: 30,
-    //         ease: SlowMo.ease.config(0.1, 0.7, false)
-    //     });
-    //     tl2.to($circlesBottomRight.eq(0), 0.1, {
-    //         scale: 0.2,
-    //         x: '-=6',
-    //         y: '+=3'
-    //     });
-    //     tl2.to($circlesBottomRight.eq(1), 0.1, {
-    //         scale: 0.8,
-    //         x: '+=7',
-    //         y: '+=3'
-    //     }, '-=0.1');
-    //     tl2.to($circlesBottomRight.eq(2), 0.1, {
-    //         scale: 0.2,
-    //         x: '+=15',
-    //         y: '-=6'
-    //     }, '-=0.2');
-    //     tl2.to($circlesBottomRight.eq(0), 1, {
-    //         scale: 0,
-    //         x: '+=5',
-    //         y: '+=15',
-    //         opacity: 0
-    //     });
-    //     tl2.to($circlesBottomRight.eq(1), 1, {
-    //         scale: 0.4,
-    //         x: '+=7',
-    //         y: '+=7',
-    //         opacity: 0
-    //     }, '-=1');
-    //     tl2.to($circlesBottomRight.eq(2), 1, {
-    //         scale: 0,
-    //         x: '+=15',
-    //         y: '-=5',
-    //         opacity: 0
-    //     }, '-=1');
-    
-    //     tlBt2.set($circlesBottomRight, {
-    //         x: 0,
-    //         y: 0,
-    //         rotation: 45
-    //     });
-    //     tlBt2.add(tl2);
-    
-    //     btTl.add(tlBt1);
-    //     btTl.to($(this).parent().find('.button.effect-button'), 0.8, {
-    //         scaleY: 1.1
-    //     }, 0.1);
-    //     btTl.add(tlBt2, 0.2);
-    //     btTl.to($(this).parent().find('.button.effect-button'), 1.8, {
-    //         scale: 1,
-    //         ease: Elastic.easeOut.config(1.2, 0.4)
-    //     }, 1.2);
-    
-    //     btTl.timeScale(2.6);
-    
-    //     $(this).on('mouseover', function () {
-    //         btTl.restart();
-    //     });
-    // });
+$(function () {
+    //刪除
+    $(".DelDia").click(function(){
+        $(this).parent('div').parent('div').parent('div').remove();
+    });
+
+    $(".DelFav").click(function(){
+        $(this).parent('div').parent('div').parent('div').remove();
+    });
 });
