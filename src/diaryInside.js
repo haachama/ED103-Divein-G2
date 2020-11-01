9e4079e4005e4d535dc12eca367f7f918d05e65d
AnnHsu <kjh06060516@gmail.com>
2020-11-02 00:40:12 +0800

    三個頁面調整

---
+++ src/diaryInside.js
@@ -0,0 +1,233 @@
+    import {TweenMax, TimelineMax} from "gsap";
+    import Vue from "vue";
+
+$(function(){
+    $('.button--bubble').each(function () {
+        var $circlesTopLeft = $(this).parent().find('.circle.top-left');
+        var $circlesBottomRight = $(this).parent().find('.circle.bottom-right');
+
+
+        var tl = new TimelineMax();
+        var tl2 = new TimelineMax();
+
+        var btTl = new TimelineMax({
+            paused: true
+        });
+
+        tl.to($circlesTopLeft, 1.2, {
+            x: -25,
+            y: -25,
+            scaleY: 2,
+            ease: SlowMo.ease.config(0.1, 0.7, false)
+        });
+        tl.to($circlesTopLeft.eq(0), 0.1, {
+            scale: 0.2,
+            x: '+=6',
+            y: '-=2'
+        });
+        tl.to($circlesTopLeft.eq(1), 0.1, {
+            scaleX: 1,
+            scaleY: 0.8,
+            x: '-=10',
+            y: '-=7'
+        }, '-=0.1');
+        tl.to($circlesTopLeft.eq(2), 0.1, {
+            scale: 0.2,
+            x: '-=15',
+            y: '+=6'
+        }, '-=0.1');
+        tl.to($circlesTopLeft.eq(0), 1, {
+            scale: 0,
+            x: '-=5',
+            y: '-=15',
+            opacity: 0
+        });
+        tl.to($circlesTopLeft.eq(1), 1, {
+            scaleX: 0.4,
+            scaleY: 0.4,
+            x: '-=10',
+            y: '-=10',
+            opacity: 0
+        }, '-=1');
+        tl.to($circlesTopLeft.eq(2), 1, {
+            scale: 0,
+            x: '-=15',
+            y: '+=5',
+            opacity: 0
+        }, '-=1');
+
+
+        var tlBt1 = new TimelineMax();
+        var tlBt2 = new TimelineMax();
+
+        tlBt1.set($circlesTopLeft, {
+            x: 0,
+            y: 0,
+            rotation: -45
+        });
+        tlBt1.add(tl);
+
+        tl2.set($circlesBottomRight, {
+            x: 0,
+            y: 0
+        });
+        tl2.to($circlesBottomRight, 1.1, {
+            x: 30,
+            y: 30,
+            ease: SlowMo.ease.config(0.1, 0.7, false)
+        });
+        tl2.to($circlesBottomRight.eq(0), 0.1, {
+            scale: 0.2,
+            x: '-=6',
+            y: '+=3'
+        });
+        tl2.to($circlesBottomRight.eq(1), 0.1, {
+            scale: 0.8,
+            x: '+=7',
+            y: '+=3'
+        }, '-=0.1');
+        tl2.to($circlesBottomRight.eq(2), 0.1, {
+            scale: 0.2,
+            x: '+=15',
+            y: '-=6'
+        }, '-=0.2');
+        tl2.to($circlesBottomRight.eq(0), 1, {
+            scale: 0,
+            x: '+=5',
+            y: '+=15',
+            opacity: 0
+        });
+        tl2.to($circlesBottomRight.eq(1), 1, {
+            scale: 0.4,
+            x: '+=7',
+            y: '+=7',
+            opacity: 0
+        }, '-=1');
+        tl2.to($circlesBottomRight.eq(2), 1, {
+            scale: 0,
+            x: '+=15',
+            y: '-=5',
+            opacity: 0
+        }, '-=1');
+
+        tlBt2.set($circlesBottomRight, {
+            x: 0,
+            y: 0,
+            rotation: 45
+        });
+        tlBt2.add(tl2);
+
+        btTl.add(tlBt1);
+        btTl.to($(this).parent().find('.button.effect-button'), 0.8, {
+            scaleY: 1.1
+        }, 0.1);
+        btTl.add(tlBt2, 0.2);
+        btTl.to($(this).parent().find('.button.effect-button'), 1.8, {
+            scale: 1,
+            ease: Elastic.easeOut.config(1.2, 0.4)
+        }, 1.2);
+
+        btTl.timeScale(2.6);
+
+        $(this).on('mouseover', function () {
+            btTl.restart();
+        });
+    });
+});
+
+$(function(){
+    // hamburger icon 的切換
+    $("button.hamburger").on("click", function () {
+        $(this).toggleClass("is-active");
+        $('.nav').slideToggle("show");
+        $('.memCart li').toggleClass("show");
+        $('header').toggleClass("bg")
+    });
+    // RWD導覽列
+    $(window).resize(function () {
+        var $pixel = document.body.clientWidth;
+
+        if ($pixel > 991) {
+            $('.nav').removeClass('hide')
+            $('.nav').addClass('show')
+        } else {
+            $('.nav').removeClass('show')
+            $('.nav').addClass('hide')
+        }
+    });
+});
+
+
+$(function(){
+    // 開啟 Modal 彈跳視窗
+    $(".mainBtn_3").on("click", function(){
+        $(".lightbox-block3").addClass("-openbox");
+    });
+    // 關閉 Modal
+    $(".btn_modal_close").on("click", function(){
+        $(".lightbox-block3").addClass("-opacity-zero");
+        // 設定隔一秒後，移除相關 class
+        setTimeout(function(){
+            $(".lightbox-block3").removeClass("-openbox -opacity-zero");
+        }, 1000);
+    });
+    $(".submitbtn3").on("click", function(){
+        $(".lightbox-block3").addClass("-opacity-zero");
+        // 設定隔一秒後，移除相關 class
+        setTimeout(function(){
+            $(".lightbox-block3").removeClass("-openbox -opacity-zero");
+        }, 1000);
+    });
+});
+
+
+        // Chart.defaults.global.responsive = true; 
+        // //Chart.js 預設為非響應式。將responsive 設true轉化為響應式圖表。設定全域性值。
+        // // line chart data 折線圖數據
+        // var buyerData = {
+        //     labels : ["時間1","時間2","時間3","時間4","時間5"],
+        //     datasets : [
+        //     {
+        //         fillColor : "rgba(172,194,132,0.4)", //透明綠
+        //         strokeColor : "#ACC26D", //綠
+        //         pointColor : "#fff", //白
+        //         pointStrokeColor : "#9DB86D", //綠
+        //         data : [203,156,99,251,305]
+        //     }
+        // ]
+        // }
+        // // get line chart canvas 獲取
+        // var buyers = document.getElementById('buyers').getContext('2d');
+        // // draw line chart 繪製
+        // new Chart(buyers).Line(buyerData);
+        
+        new Vue({
+                el: '.pageLeftLine',
+                data() {
+                    return {
+                    data: [10, 71, 78, 25, 36, 92],
+                    line: '',
+                    };
+                },
+                mounted() {
+                    this.calculatePath();
+                },
+                methods: {
+                    getScales() {
+                    const x = d3.scaleTime().range([0, 500]);
+                    const y = d3.scaleLinear().range([210, 0]);
+                    d3.axisLeft().scale(x);
+                    d3.axisBottom().scale(y);
+                    x.domain(d3.extent(this.data, (d, i) => i));
+                    y.domain([0, d3.max(this.data, d => d)]);
+                    return { x, y };
+                    },
+                    calculatePath() {
+                    const scale = this.getScales();
+                    const path = d3.line()
+                        .x((d, i) => scale.x(i))
+                        .y(d => scale.y(d));
+                    this.line = path(this.data);
+                    },
+                },
+            });
