af3c7471f4074bd24b335335658bb33467139d23
dauren0260 <violet0260@gmail.com>
2020-11-03 01:27:43 +0800

    首頁完成

--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
 import $ from 'jquery';
+import Parallax from 'parallax-js';
+import TimelineMax from 'gsap';
+
 
 $(function () {
 
     // hamburger icon 的切換
     $("button.hamburger").on("click", function () {
         $(this).toggleClass("is-active");
-        $('.nav').slideToggle("show");
+        $('.nav').toggleClass("show");
         $('.memCart li').toggleClass("show");
         $('header').toggleClass("bg")
     });
@@ -22,4 +25,205 @@ $(function () {
         }
     });
 
+    $('.memCart li:first-child').addClass("hide");
+
+
+    // bubbleBtn
+
+    const {
+        TweenMax,
+        TimelineMax
+    } = require("gsap");
+
+    $('.button--bubble').each(function () {
+        var $circlesTopLeft = $(this).parent().find('.circle.top-left');
+        var $circlesBottomRight = $(this).parent().find('.circle.bottom-right');
+
+        var tl = new TimelineMax();
+        var tl2 = new TimelineMax();
+
+        var btTl = new TimelineMax({
+            paused: true
+        });
+
+        tl.to($circlesTopLeft, 1.2, {
+            x: -25,
+            y: -25,
+            scaleY: 2,
+            ease: SlowMo.ease.config(0.1, 0.7, false)
+        });
+        tl.to($circlesTopLeft.eq(0), 0.1, {
+            scale: 0.2,
+            x: '+=6',
+            y: '-=2'
+        });
+        tl.to($circlesTopLeft.eq(1), 0.1, {
+            scaleX: 1,
+            scaleY: 0.8,
+            x: '-=10',
+            y: '-=7'
+        }, '-=0.1');
+        tl.to($circlesTopLeft.eq(2), 0.1, {
+            scale: 0.2,
+            x: '-=15',
+            y: '+=6'
+        }, '-=0.1');
+        tl.to($circlesTopLeft.eq(0), 1, {
+            scale: 0,
+            x: '-=5',
+            y: '-=15',
+            opacity: 0
+        });
+        tl.to($circlesTopLeft.eq(1), 1, {
+            scaleX: 0.4,
+            scaleY: 0.4,
+            x: '-=10',
+            y: '-=10',
+            opacity: 0
+        }, '-=1');
+        tl.to($circlesTopLeft.eq(2), 1, {
+            scale: 0,
+            x: '-=15',
+            y: '+=5',
+            opacity: 0
+        }, '-=1');
+
+        var tlBt1 = new TimelineMax();
+        var tlBt2 = new TimelineMax();
+
+        tlBt1.set($circlesTopLeft, {
+            x: 0,
+            y: 0,
+            rotation: -45
+        });
+        tlBt1.add(tl);
+
+        tl2.set($circlesBottomRight, {
+            x: 0,
+            y: 0
+        });
+        tl2.to($circlesBottomRight, 1.1, {
+            x: 30,
+            y: 30,
+            ease: SlowMo.ease.config(0.1, 0.7, false)
+        });
+        tl2.to($circlesBottomRight.eq(0), 0.1, {
+            scale: 0.2,
+            x: '-=6',
+            y: '+=3'
+        });
+        tl2.to($circlesBottomRight.eq(1), 0.1, {
+            scale: 0.8,
+            x: '+=7',
+            y: '+=3'
+        }, '-=0.1');
+        tl2.to($circlesBottomRight.eq(2), 0.1, {
+            scale: 0.2,
+            x: '+=15',
+            y: '-=6'
+        }, '-=0.2');
+        tl2.to($circlesBottomRight.eq(0), 1, {
+            scale: 0,
+            x: '+=5',
+            y: '+=15',
+            opacity: 0
+        });
+        tl2.to($circlesBottomRight.eq(1), 1, {
+            scale: 0.4,
+            x: '+=7',
+            y: '+=7',
+            opacity: 0
+        }, '-=1');
+        tl2.to($circlesBottomRight.eq(2), 1, {
+            scale: 0,
+            x: '+=15',
+            y: '-=5',
+            opacity: 0
+        }, '-=1');
+
+        tlBt2.set($circlesBottomRight, {
+            x: 0,
+            y: 0,
+            rotation: 45
+        });
+        tlBt2.add(tl2);
+
+        btTl.add(tlBt1);
+        btTl.to($(this).parent().find('.button.effect-button'), 0.8, {
+            scaleY: 1.1
+        }, 0.1);
+        btTl.add(tlBt2, 0.2);
+        btTl.to($(this).parent().find('.button.effect-button'), 1.8, {
+            scale: 1,
+            ease: Elastic.easeOut.config(1.2, 0.4)
+        }, 1.2);
+
+        btTl.timeScale(2.6);
+
+        $(this).on('mouseover', function () {
+            btTl.restart();
+        });
+    });
+
+
+    // courseList
+    $('.post-module').hover(function () {
+        $(this).find('.description').stop().animate({
+            height: "toggle",
+            opacity: "toggle"
+        }, 100);
+    });
+
 });
+
+
+// parallax fish
+var scene = document.getElementById('scene');
+var parallax = new Parallax(scene);
+
+$('.fishBallon').css({
+    top: '130px',
+    left: '30px',
+    display: 'inline-block'
+})
+
+$('.fishTurtle').css({
+    top: '100px',
+    left: '350px',
+})
+
+$('.fishGold').css({
+    top: '190px',
+    left: '150px',
+})
+
+$('.fishNemo').css({
+    top: '200px',
+    left: '450px',
+})
+
+var pixel = document.body.clientWidth;
+
+window.addEventListener('resize', mobileFish());
+
+function mobileFish() {
+    if (pixel < 991) {
+        $('.fishNemo').css({
+            display: 'none',
+        })
+        $('.fishTurtle').css({
+            top: '150px',
+            left: '250px'
+        })
+    } else {
+        $('.fishNemo').css({
+            display: 'inline-block',
+        })
+        $('.fishTurtle').css({
+            top: '100px',
+            left: '350px'
+        })
+    }
+}
+
+mobileFish();
