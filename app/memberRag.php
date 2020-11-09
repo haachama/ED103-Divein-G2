772a5a11d85a63474cbcdeb0d49ef87a2c3c0b49
f94baa176 <f94baasd176ing@gmail.com>
2020-11-09 11:39:08 +0800

    20201109

--- app/memberRag.php
+++ app/memberRag.php
@@ -3,11 +3,11 @@ try{
     require_once("./connectBooks.php");
     $sql = "insert into `member` set memberId=:memberId, memberPsw=:memberPsw, memberName=:memberName, memberNickName=:memberNickName, memMail=:memMail";
     $member = $pdo->prepare($sql);
-    $member->bindValue(":RegMemId", $_POST["memberId"]);
-    $member->bindValue(":RegMemPsw", $_POST["memberPsw"]);
-    $member->bindValue(":RegMemName", $_POST["memberName"]);
-    $member->bindValue(":RegMemNickName", $_POST["memberNickName"]);
-    $member->bindValue(":RegMemMail", $_POST["memMail"]);
+    $member->bindValue(":memberId", $_POST["RegMemId"]);
+    $member->bindValue(":memberPsw", $_POST["RegMemPsw"]);
+    $member->bindValue(":memberName", $_POST["RegMemName"]);
+    $member->bindValue(":memberNickName", $_POST["RegMemNickName"]);
+    $member->bindValue(":memMail", $_POST["RegMemMail"]);
     $member->execute();
 }catch(PDOException $e){
 	$error = array("errorMsg"=>$e->getMessage());
