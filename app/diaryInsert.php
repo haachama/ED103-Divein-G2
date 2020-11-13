82a8ce8b51de05bd4cef7b462adaa7337495a3c0
annhsu <kjh06060516@gmail.com>
2020-11-13 22:01:21 +0800

    add OK

--- app/diaryInsert.php
+++ app/diaryInsert.php
@@ -1,6 +1,7 @@
 <?php
+    // $error = '';
 
-    $diaryName = isset($_POST["diaryName"])?$_POST["diaryName"]:""; //尋找是否回傳數字回來，(html的class)有的話寫入資料，沒有寫入空字串
+    $diaryName = isset($_POST["diaryName"])?$_POST["diaryName"]:""; 
     $diaryType = isset($_POST["diaryType"])?$_POST["diaryType"]:"";
     $diveNo = isset($_POST["diveNo"])?$_POST["diveNo"]:"";
     $diaryDiveTime = isset($_POST["diaryDiveTime"])?$_POST["diaryDiveTime"]:"";
@@ -15,8 +16,23 @@
     $diaryPoint3 = isset($_POST["diaryPoint3"])?$_POST["diaryPoint3"]:"";
     $diaryPoint4 = isset($_POST["diaryPoint4"])?$_POST["diaryPoint4"]:"";
     $diaryPoint5 = isset($_POST["diaryPoint5"])?$_POST["diaryPoint5"]:"";
-
-    echo $diaryPoint5;
+    // echo $diaryName,'<br>';
+    // echo $diaryType,'<br>';
+    // echo $diveNo,'<br>';
+    // echo $diaryDiveTime,'<br>';
+    // echo $diaryPlayDate,'<br>';
+    // echo $diaryWeather,'<br>';
+    // echo $diaryVisibility,'<br>';
+    // echo $diaryTemp,'<br>';
+    // echo $diaryWaterTemp,'<br>';
+    // echo $diaryText,'<br>';
+    // echo $diaryPoint1,'<br>';
+    // echo $diaryPoint2,'<br>';
+    // echo $diaryPoint3,'<br>';
+    // echo $diaryPoint4,'<br>';
+    // echo $diaryPoint5,'<br>';
+    // echo '<br>';
+    // echo $diaryPoint5;
     // die;
     //測試MYSQL
     // INSERT INTO personaldiary(memNo,diaryName,diaryType,diaryWriteDate,diveNo,diaryDiveTime,diaryPlayDate,diaryWeather,diaryVisibility,diaryTemp,diaryWaterTemp,diaryText,diaryPoint1,diaryPoint2,diaryPoint3,diaryPoint4,diaryPoint5,diaryPicsNo) 
@@ -24,42 +40,83 @@
 
 
 try{
-    $dsn = "mysql:host=localhost;port=3308;dbname=ed103g2;charset=utf8";  //家3308 教室3306
+    $dsn = "mysql:host=localhost;port=3306;dbname=ed103g2;charset=utf8";  //家3308 教室3306
     $user = "root";
-    $password = "";//家no 教室root 
-    $options = array(PDO::ATTR_CASE => PDO::CASE_NATURAL, PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION);
+    $password = "root";//家no 教室root 
+    $options = array(PDO::ATTR_CASE => PDO::CASE_NATURAL,PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION);
     $pdo = new PDO($dsn, $user, $password, $options);
     
-     //第一個是sql欄位
-    
-    $sql = "INSERT INTO personaldiary(memNo,diaryName,diaryType,diaryWriteDate,diveNo,diaryDiveTime,diaryPlayDate,diaryWeather,
-    diaryVisibility,diaryTemp,diaryWaterTemp,diaryText,diaryPoint1,diaryPoint2,diaryPoint3,diaryPoint4,diaryPoint5,diaryPicsNo) 
-            VALUES(1,$diaryName,$diaryType,now(),$diveNo,$diaryDiveTime,$diaryPlayDate,$diaryWeather,$diaryVisibility,$diaryTemp,$diaryWaterTemp,$diaryText,$diaryPoint1,$diaryPoint2,$diaryPoint3,$diaryPoint4,$diaryPoint5,'diaryphoto1.png')"; 
-    // 1是寫死的會員
+    //第一個是sql欄位
+    // NOTNULL  memNo、diaryName、diaryWriteDate、diveNo
+    $sql = "INSERT INTO personaldiary(
+                memNo,
+                diaryName,
+                diaryType,
+                diaryWriteDate,
+                diveNo,
+                diaryDiveTime,
+                diaryPlayDate,
+                diaryWeather,
+                diaryVisibility,
+                diaryTemp,
+                diaryWaterTemp,
+                diaryText,
+                diaryPoint1,
+                diaryPoint2,
+                diaryPoint3,
+                diaryPoint4,
+                diaryPoint5,
+                diaryPicsNo) 
+            VALUES(
+                1,
+                '$diaryName',
+                $diaryType,
+                now(),
+                $diveNo,
+                $diaryDiveTime,
+                '$diaryPlayDate',
+                $diaryWeather,
+                $diaryVisibility,
+                $diaryTemp,
+                $diaryWaterTemp,
+                '$diaryText',
+                $diaryPoint1,
+                $diaryPoint2,
+                $diaryPoint3,
+                $diaryPoint4,
+                $diaryPoint5,
+                'img/diary/diaryphoto1.png')";
+
+
+    // 1是寫死的會員，上傳圖片也是死的
     // echo $diaryName,$diaryType,$diveNo,$diaryDiveTime,$diaryPlayDate,$diaryWeather,$diaryVisibility,$diaryTemp,$diaryWaterTemp,$diaryText,$diaryPoint1,$diaryPoint2,$diaryPoint3,$diaryPoint4,$diaryPoint5,$fileBtn;
     // die;
     $diary = $pdo->prepare($sql);
+
     $diary->execute(); //程式執行到這結束
     $diaryNo = $pdo->lastInsertId();  //接收到剛剛的日誌流水號
 
-// -------------------------------------------------------------------------------------------------
 
-    $checkbox1 = $_POST['biologicalNo'] ;  //假設回傳是陣列  ajax的value
-    
-    foreach($checkbox1 as $i=>$biologicalNo){
-        $diaryImg->bindValue(":biologicalNo", $biologicalNo);
-        $diary2->execute(); //迴圈跑完結束 放這??
-    }
+// -------------------------------------------------------------------------------------------------
 
     $sql2 = "INSERT INTO diaryImg(biologicalNo, diaryNo) VALUES(:biologicalNo, $diaryNo)"; //還沒執行?
     $diaryImg = $pdo->prepare($sql2); //還沒執行?
-    //--------------
+
+    $checkbox1 = $_POST['biologicalNo'] ;  //假設回傳是陣列  ajax的value
+    // echo $checkbox1;
+    foreach($checkbox1 as $i=>$biologicalNo){
+        $diaryImg->bindValue(":biologicalNo", $biologicalNo);
+        $diaryImg->execute(); //迴圈跑完結束 放這??
+    }
+
     
 
     // header('Location : diaryInside.html'); //回到diaryInside.html
 
 }catch(PDOException $e){
     $error = array("errorMsg"=>$e->getMessage());
+    // $error.= $e->getLine() . '<br>' . $e->getMessage() ;
     echo json_encode($error);
 }
+// echo $diaryPlayDate;
 ?>
